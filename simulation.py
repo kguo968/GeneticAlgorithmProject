f0630ab6e42d1d07fa9f3c82189b66966766fdb6
Kevin Guo <kguo968@aucklanduni.ac.nz>
2020-03-06 19:01:05 +1300

    Added new simulation parameters

--- simulation.py
+++ simulation.py
@@ -1,22 +1,41 @@
 from ga import genetic_algorithm
 
-gen_number = []
-sample_size = 10
+#gen_number = []
+sample_size = 100
+compute_base = False
 
 # Create new textfile to store the results
-f = open("results.txt", "w+")
+if compute_base:   # Only generate new baseline_results is requested to do so 
+    f = open("baseline_results.txt", "w+")
+
+g = open("compare_results.txt", "w+")
+
 
 for i in range(sample_size):
 
     # Run the GA + add it to the list (can remove when confirm writing to textfile works as intended)
-    results = genetic_algorithm(pop_size=20)
-    gen_number.append(results)
+    if compute_base:
+        # Only run baseline GA if specified
+        results = genetic_algorithm(pop_size=20)
+        
+        if (i+1 == sample_size):
+            f.write("%d" %results)
+        else:
+            f.write("%d\n" %results)
+        
+    
+    results2 = genetic_algorithm(pop_size=100)
+    #gen_number.append(results)
     
     # Write the results of each generation to the textfile
     if (i+1 == sample_size):
-        f.write("%d" %results)
+        g.write("%d" %results2)
     else:
-        f.write("%d\n" %results)
-f.close()
- 
-print(gen_number)
+        g.write("%d\n" %results2)
+
+if compute_base:
+    f.close()
+
+g.close()
+
+#print(gen_number)
